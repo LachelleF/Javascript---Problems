7e3b09b62bd49555fc0ff49141c05ce16b447bee
Lachelle Fryett <lachellefryett@gmail.com>
2019-05-22 07:25:35 -0400

    lesson 3 adding new file

---
+++ BuildingBlocks/some problem lesson 3.js
@@ -0,0 +1,19 @@
+//==========================================================
+//------------------------Problem09-------------------------
+//==========================================================
+
+/*
+Write a function, intersection, that takes two arrays and returns a new array containing 
+only the values that are in both arrays.
+a:  check array 1 elements against array 2
+ i: two or more arrays
+o: a union or intersection of shared elements
+e: one array might be longer than the other
+*/
+function intersection (array1, array2) {
+
+
+
+
+//intersection(['a', 'b', 'c', 'f', 'g'], ['d', 'b', 'c', 'd', 'e'])); // --> ['b', 'c']
+intersection([1, 4, 6, 2], [10, 9, 4, 1, 5, 6]); // --> [1, 4, 6]
